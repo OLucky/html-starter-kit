cc16e70a8d814552faaf24b25810da725d830799
Georgii Ishchenko <ge.ishchenko@gmail.com>
2018-10-16 17:06:31 +0300

    Fix html-webpack-plugin config generator for Mac

--- html-webpack-plugin.config.js
+++ html-webpack-plugin.config.js
@@ -1,34 +1,35 @@
-const fs = require("fs");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const defaultPath = `${__dirname}/src/templates`;
-const initialFolderName = "pages";
+const initialFolderName = 'pages';
 
 const pluginConfig = [];
 
 const getTemplateConfigs = (folderName = initialFolderName, prevPath = defaultPath) => {
-	const path = `${prevPath}/${folderName}`;
+  const path = `${prevPath}/${folderName}`;
 
-	const templateFiles = fs.readdirSync(path);
+  const templateFiles = fs.readdirSync(path);
 
-	templateFiles.forEach(fullName => {
-		const stats = fs.statSync(`${path}/${fullName}`);
-		if(!stats.isDirectory()) {
-	
-			const nameWithoutExtension = fullName.replace(/\.[^/.]+$/, "");
-			const subfolders = folderName !== initialFolderName ? 
-				path.substr(path.indexOf(`${initialFolderName}`) + initialFolderName.length) + "/" : "";
-				
-			pluginConfig.push(new HtmlWebpackPlugin({
-				filename: `${subfolders}${nameWithoutExtension}.html`,
-				template: `${path}/${fullName}`
-			}));
+  templateFiles.forEach(fullName => {
+    const stats = fs.statSync(`${path}/${fullName}`);
+    if (!stats.isDirectory()) {
+      const nameWithoutExtension = fullName.replace(/\.[^/.]+$/, '');
+      const subfolders =
+        folderName !== initialFolderName
+          ? '.' + path.substr(path.indexOf(`${initialFolderName}`) + initialFolderName.length) + '/'
+          : './';
 
-		}
-		else {
-			getTemplateConfigs(fullName, path);
-		}
-	});
+      pluginConfig.push(
+        new HtmlWebpackPlugin({
+          filename: `${subfolders}${nameWithoutExtension}.html`,
+          template: `${path}/${fullName}`
+        })
+      );
+    } else {
+      getTemplateConfigs(fullName, path);
+    }
+  });
 };
 
 getTemplateConfigs();
